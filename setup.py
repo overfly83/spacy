4e587bc813263b829107859eb5ce651011fc80b5
Edward Zhang <edward.zhang@sap.com>
2018-12-14 17:31:14 +0800

    add init files

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+from os.path import abspath, join, dirname
+
+CURDIR = dirname(abspath(__file__))
+execfile(join(CURDIR, 'version.py'))
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="an_example_pypi_project",
+    version="0.0.1",
+    author="Edward Zhang",
+    author_email="edward.zhang@sap.com",
+    description="An project to test spacy model",
+    license="MIT",
+    keywords="example documentation tutorial",
+    url="http://packages.python.org/an_example_pypi_project",
+    packages=['an_example_pypi_project', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
