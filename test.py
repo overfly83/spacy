8bd219bbd71b74aa5480340c3396a040baf3ceff
Edward Zhang <edward.zhang@sap.com>
2018-12-17 17:42:02 +0800

    add django init

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-import os
-from setuptools import setup
-from os.path import abspath, join, dirname
+import en_core_web_sm
 
-if __name__ == '__main__':
-    CURDIR = dirname(abspath(__file__))
-    with open(join(CURDIR, 'version.py'), 'r') as f:
-        version=exec(f.read())
-        print(VERSION)
+nlp = en_core_web_sm.load()
+doc = nlp(u'lock the actual data in year 2016.')
+
+for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+          token.shape_, token.is_alpha, token.is_stop)
