4b04eacdd8d95c8f55d4a2e9f3599ac39bd26c92
Edward Zhang <edward.zhang@sap.com>
2018-12-21 17:45:15 +0800

    poc code in test

--- test.py
+++ test.py
@@ -25,24 +25,41 @@ def cleanup(token, lower=True):
 
 
 def entity_recognization(document):
-    print(document.ents)
-    labels = set([w.label_ for w in document.ents])
+    labels = set([ent.label_ for ent in document.ents])
+    print(labels)
     for label in labels:
         entities = [cleanup(e.string, lower=False) for e in document.ents if label == e.label_]
         entities = list(set(entities))
-        print(label, entities)
+        print('label, entities: ', label, entities)
 
 
 def generate_noun(document):
     for np in document.noun_chunks:
-        print(np.text, np.root.dep_, np.root.head.text)
+        print(np.text, np.root.dep_, np.root.head.text, np.sent.ents)
 
 
 if __name__ == '__main__':
+    print(u'=========================================')
     doc = nlp(u'Please help me quickly lock the cell.')
+    entity_recognization(doc)
+    generate_noun(doc)
+    print(u'=========================================')
     doc = nlp(u'Autonomous cars shift insurance liability toward manufacturers')
+    entity_recognization(doc)
+    generate_noun(doc)
+    print(u'=========================================')
+    doc = nlp(u'Lock the cell of year 2016')
+    entity_recognization(doc)
+    generate_noun(doc)
+    print(u'=========================================')
+    doc = nlp(u'Lock the cell from year 2016 to year 2018')
+    entity_recognization(doc)
+    generate_noun(doc)
+    print(u'=========================================')
+    doc = nlp(u'Lock the cell of actual data in 2018')
+    entity_recognization(doc)
+    generate_noun(doc)
     # for token in doc:
     #     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
     #           token.shape_, token.is_alpha, token.is_stop)
-    entity_recognization(doc)
-    generate_noun(doc)
+
