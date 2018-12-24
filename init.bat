f2cb37fa25630e0eb1c4d94a04d376acbf08c5e3
edward.zhang <zhl830905@gmail.com>
2018-12-25 00:09:42 +0800

    add custom

--- init.bat
+++ init.bat
@@ -3,9 +3,8 @@ IF NOT EXIST env (
 	echo "Install virtual environment."
 	pip3 install virtualenv
 	py -m virtualenv env
-	venv\Scripts\activate
 ) else (
 	echo "Skip installing virtualenv."
 )
-python setup.py install
-python -m spacy download en_core_web_sm
+call env\Scripts\python setup.py install
+call env\Scripts\python -m spacy download en_core_web_sm
